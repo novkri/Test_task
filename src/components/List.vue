7df503da3862406aa152b25eb57f44f2c20abeb0
novkri <nov.kri@yandex.ru>
2020-07-16 23:47:23 +0700

    верстка

--- src/components/List.vue
+++ src/components/List.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="container" id="app" v-cloak>
+  <div class="container" id="app" >
     <Popup 
       v-if="isShowPopupVisible"
       @closePopup="closeAddPopup"
@@ -7,28 +7,31 @@
     >
     
     </Popup>
-  <!-- <h1>Заголовок</h1> -->
-  <table class="people__table">
-    <thead class="people__table-header">
-      <tr>
-        <th>First Name</th>
-        <th>Last Name</th>
-        <th>Skills</th>
-        <th>Edit</th>
-        <th>Delete</th>
-      </tr>
-    </thead>
-    <tbody class="people__table-body">
-      <tr v-for="person in people" :key="person.id" >
-        <td>{{ person.fname}}</td>
-        <td>{{ person.lname}}</td>
-        <td>{{ person.skills}}</td>
-        <td><button @click="editPerson(person.id)" class="people__table-edit">Edit</button></td>
-        <td><button @click="removePerson(person.id)" class="people__table-delete">Delete</button></td>
-      </tr>
-    </tbody>
-  </table>
-  <button class="show__popup" @click="showPopupAdd">Создать</button>
+  <!-- <h1 class="header">Заголовок</h1> -->
+  <div class="wrapper">
+    <table class="table">
+      <thead>
+        <tr>
+          <th>First Name</th>
+          <th>Last Name</th>
+          <th>Skills</th>
+          <th>Edit</th>
+          <th>Delete</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="person in people" :key="person.id" >
+          <td data-title="First Name">{{ person.fname}}</td>
+          <td data-title="Last Name">{{ person.lname}}</td>
+          <td data-title="Skills">{{ person.skills}}</td>
+          <td data-title="Edit"><button @click="editPerson(person.id)" class="form__btn edit-btn">Edit</button></td>
+          <td data-title="Delete"><button @click="removePerson(person.id)" class="form__btn delete-btn">Delete</button></td>
+        </tr>
+      </tbody>
+    </table>
+    <button class="show__popup" @click="showPopupAdd">Создать</button>
+  </div> <!-- div.wrapper -->
+  <!-- <button class="show__popup" @click="showPopupAdd">Создsать</button> -->
 </div>
 </template>
 
@@ -94,8 +97,131 @@ export default {
 </script>
 
 <style>
-td {
-  padding: 8px;
+* {
+  box-sizing: border-box;
+  -webkit-box-sizing: border-box;
+  -moz-box-sizing: border-box;
+  -o-box-sizing: border-box;
 }
 
+body {
+  margin: 0;
+  padding: 20px;
+  font-family: Arial, "Helvetica Neue", Helvetica, sans-serif;
+}
+
+/* .wrapper {
+  background-color: blueviolet;
+} */
+
+.table {
+  width: 100%;
+  border-collapse: collapse;
+}
+
+.table td,
+.table th {
+  padding: 12px 15px;
+  border: 1px solid #ddd;
+  text-align: center;
+}
+
+.table th {
+  background-color: rgba(105, 105, 105, 0.562);
+  color: #fff;
+  font-size: 16px;
+}
+
+.table tbody tr:nth-child(even){
+  background-color: #f5f5f5;
+}
+
+@media screen and (max-width: 576px) {
+  .table thead {
+    display: none;
+  }
+
+  .table, .table tbody, .table tr, .table td {
+    display: block;
+    width: 100%;
+  }
+
+  .table tr {
+    margin-bottom: 15px;
+  }
+
+  .table td {
+    text-align: right;
+    padding-left: 50%;
+    position: relative;
+  }
+
+  .table td::before {
+    content: attr(data-title);
+    position: absolute;
+    left: 0;
+    width: 50%;
+    padding-left: 15px;
+    font-size: 15px;
+    font-weight: bold;
+    text-align: left;
+  }
+}
+
+
+.show__popup {
+  padding: 15px 35px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 16px;
+
+  margin-top: 25px;
+  
+  -webkit-transition-duration: 0.4s; 
+  -moz-transition-duration: 0.4s;
+  -o-transition-duration: 0.4s;
+  transition-duration: 0.4s;
+
+  background-color: white;
+  color: black;
+  border: 2px solid #4CAF50;
+  border-radius: 5px;
+}
+
+.show__popup:hover, .show__popup:active {
+    background-color: #4CAF50; 
+    color: white;
+}
+
+@media screen and (max-width: 576px) {
+  .header {
+    visibility: hidden;
+  }
+  .show__popup {
+    position: absolute;
+    top: 0;
+    right: 21px;
+  }
+}
+
+.form__btn {
+  padding: 10px 15px;
+
+  -webkit-transition-duration: 0.4s; 
+  -moz-transition-duration: 0.4s;
+  -o-transition-duration: 0.4s;
+  transition-duration: 0.4s;
+
+  background-color: white;
+  color: black;
+  border: 1.5px solid #9e9e9e;
+  border-radius: 5px;
+}
+.edit-btn:hover, .edit-btn:active {
+  background-color: rgb(174, 181, 248);
+}
+.delete-btn:hover, .delete-btn:active  {
+  background-color: rgb(253, 131, 131);
+}
 </style>
--- src/components/Popup.vue
+++ src/components/Popup.vue
@@ -5,29 +5,37 @@
         <div class="popup__container">
           <div class="popup__header">
             <span>Popup name</span>
-            <span>
-              <i
-                class="material-icons"
-                @click="closePopup"
-              >close</i>
-            </span>
+            <span class="close" @click="closePopup"></span>
           </div>
           <div class="popup__content">
-              <form class="form-inline" @submit="createPerson">
-                <div class="form-group">
+              <form @submit="createPerson">
+                <div class="row">
+                  <div class="col-25">
                     <label for="firstname">firstname</label>
-                    <input type="text" class="form-control" id="firstname" v-model="person.fname">
+                  </div>
+                  <div class="col-75">
+                    <input type="text" id="firstname" v-model="person.fname">
+                  </div>
                 </div>
-                <div class="form-group">
+                <div class="row">
+                  <div class="col-25">
                     <label for="lastname">lastname</label>
-                    <input type="text" class="form-control" id="lastname" v-model="person.lname">
+                  </div>
+                  <div class="col-75">
+                    <input type="text" id="lastname" v-model="person.lname">
+                  </div>
                 </div>
-                <div class="form-group">
-                    <label for="skills">skill...</label>
-                    <input type="text" class="form-control" id="skills" v-model="person.skills">
+                <div class="row">
+                  <div class="col-25">
+                    <label for="skills">Skills</label>
+                  </div>
+                  <div class="col-75">
+                    <input type="text" id="skills" v-model="person.skills">
+                  </div>
                 </div>
-
-                <button class="btn btn-primary" type="submit" :disabled="!canCreate">Создать</button>
+                <div class="row">
+                  <button class="form-btn" type="submit" :disabled="!canCreate">Создать</button>
+                </div>
             </form>
 
             <slot></slot>
@@ -81,8 +89,6 @@ export default {
 </script>
 
 <style scoped>
-
-
 .popup {
   position: fixed;
   z-index: 9998;
@@ -93,7 +99,10 @@ export default {
   background-color: rgba(0, 0, 0, 0.5);
   display: table;
   transition: opacity 0.3s ease;
-
+  -webkit-transition: opacity 0.3s ease; 
+  -moz-transition: opacity 0.3s ease; 
+  -o-transition: opacity 0.3s ease;
+  /* letter-spacing: 1px; */
 }
 
 .popup__wrapper {
@@ -102,39 +111,136 @@ export default {
 }
 
 .popup__container {
-  width: 300px;
+  width: 400px;
   margin: 0px auto;
   padding: 20px 30px;
+  font-size: 20px;
   background-color: #fff;
   border-radius: 2px;
   box-shadow: 0 2px 8px rgba(0, 0, 0, 0.33);
+  -webkit-transition: all 0.3s ease;
+  -moz-transition: all 0.3s ease;
+  -o-transition: all 0.3s ease;
   transition: all 0.3s ease;
-  font-family: Helvetica, Arial, sans-serif;
+  /* font-family: Helvetica, Arial, sans-serif; */
+}
+
+@media screen and (max-width: 576px) {
+  .popup__container {
+    width: 300px;
+    font-size: 18px;
+  }
+
+  .popup__header {
+    font-size: 20px;
+  }
 }
 
 .popup__header {
   margin-top: 0;
-  color: #42b983;
+  font-size: 22px;
+  font-weight: bold;
   display: flex;
   justify-content: space-between;
   align-items: center;
 }
 
-.popup__container-body {
-  margin: 20px 0;
-}
-
-.popup__header i {
-  cursor: pointer;
-  float: right;
-}
-
-form {
-  padding: 5px;
-}
-
 input {
-  margin-bottom: 5px;
+  width: 100%;
+  padding: 12px;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+  /* box-sizing: border-box; */
+  /* resize: vertical; */
+}
+
+label {
+  padding: 12px 0;
+  display: inline-block;
+}
+
+.form-btn {
+  background-color: #4CAF50;
+  font-size: 16px;
+  color: white;
+  padding: 12px 20px;
+  border: none;
+  border-radius: 4px;
+  cursor: pointer;
+  margin-top: 10px;
+  letter-spacing: 1px;
+}
+
+.form-btn:disabled {
+  background-color: #b3b3b3;
+}
+
+.popup__content {
+  border-radius: 5px;
+}
+
+.col-25 {
+  float: left;
+  width: 30%;
+  margin-top: 6px;
+}
+
+.col-75 {
+  float: left;
+  width: 70%;
+  margin-top: 6px;
+}
+
+.row:after {
+  content: "";
+  display: table;
+  clear: both;
+}
+
+@media screen and (max-width: 576px) {
+  .col-25, .col-75, input {
+    width: 100%;
+    margin-top: 0;
+  }
+
+  label {
+    padding: 11px 0 4px;
+  }
+}
+
+.close {
+  position: relative;
+  width: 35px;
+  height: 37px;
+  opacity: 0.3;
+  cursor: pointer;
+}
+
+.close:hover {
+  opacity: 1;
+}
+
+.close:before, .close:after {
+  position: absolute;
+  left: 15px;
+  content: ' ';
+  height: 33px;
+  width: 2px;
+  background-color: #333;
+}
+
+.close:before {
+  transform: rotate(45deg);
+  -webkit-transform: rotate(45deg);
+  -moz-transform: rotate(45deg);
+  -o-transform: rotate(45deg);
+}
+
+.close:after {
+  transform: rotate(-45deg);
+  -webkit-transform: rotate(-45deg);
+  -moz-transform: rotate(-45deg);
+  -o-transform: rotate(-45deg);
 }
 
 </style>
